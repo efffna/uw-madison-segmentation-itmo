1300b16dd2901d1baf87bceb3b1ca88db3d61cdb
efffna <69052021+efffna@users.noreply.github.com>
2022-06-30 14:51:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from bdb import set_trace
 from tools import Segmentation
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, Response
-import aiofiles
 from typing import List
+import os
 
-PATH_OUT = 'temp/out.png'
-PATH_GIF = 'temp/1.gif'
 
+PATH_OUT = 'out.gif'
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"],
                    allow_headers=["*"])
-
 img = Segmentation()
 
-
 @app.post("/upload_files")
-async def upload_files(files: List[UploadFile] = File(...)):
-    for file in files:
-        print(file)
-        
+async def upload(files: List[UploadFile] = File(...)):
+    count = 0
+    for img_remove in [i for i in os.listdir('temp') if i.endswith('.png')]:
+        os.remove('temp/'+img_remove)
+    for file in files :
+        contents = await file.read()
+        img.preprocessing(contents)
+        img.get_predict()
+        os.rename('temp/out.png', f'temp/out{count}.png')
+        count += 1
+    img.gif_create()
 
 @app.get("/get_mask")
 def get_img():    
